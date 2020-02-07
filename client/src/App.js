21a9d0281717d80095cf13af94c94130a708bba5
Andy Hoffman <antibland@gmail.com>
2020-02-06 17:25:55 -0800

    Working demo for interview

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 
 function App() {
-  const [renderedResponse, setRenderedResponse] = useState('');
+  const [renderedResponse, setRenderedResponse] = useState([])
+  const [imgCache, setImageCache] = useState([]);
+  const [imgIndex, setImageIndex] = useState(0);
+  const fetchURL = "https://api.thecatapi.com/v1/images/search";
 
   const getResponse = async () => {
-    const response = await fetch('/api/hello')
+    const response = await fetch(fetchURL);
     const body = await response.json()
     if (response.status !== 200) throw Error(body.message)
 
@@ -15,14 +18,79 @@ function App() {
   useEffect(() => {
     getResponse()
       .then(res => {
-        console.log(res)
-        setRenderedResponse(res.express);
+        setRenderedResponse(res);
+        pushToCache(res[0]);
       })
   }, []);
 
+  function MainContent({ children }) {
+    return (
+      <main>{children}</main>
+    )
+  }
+
+  function handlePrev() {
+    setImageIndex(imgIndex - 1);
+  }
+
+  function handleNext() {
+    setImageIndex(imgIndex + 1);
+  }
+
+  function pushToCache(obj) {
+    setImageCache([...imgCache, obj]);
+  }
+
+  function handleRand() {
+    getResponse()
+      .then(res => {
+        setRenderedResponse(res);
+        pushToCache(res[0]);
+      }).finally(() => {
+        setImageIndex(imgCache.length);
+      })
+  }
+
+  function Buttons() {
+    const cache = imgCache;
+
+    return (
+      <div className="buttonsContainer">
+        <button disabled={imgIndex == 0} onClick={handlePrev}>Prev</button>
+        <button onClick={handleRand}>Random</button>
+        <button disabled={imgIndex == cache.length - 1} onClick={handleNext}>Next</button>
+      </div>
+    )
+  }
+
+  function Response() {
+    const { url, breed, lifeSpan } = imgCache[imgIndex];
+
+    return (
+      <div className="container">
+        <div className="top">
+          <figure className="imgContainer">
+            <img src={url} alt="" />
+          </figure>
+          <dl>
+            <dt>Breed:</dt>
+            <dd>{breed}</dd>
+            <dt> Life Span:</dt>
+            <dd>{lifeSpan}</dd>
+            <dt>Url:</dt>
+            <dd>{url}</dd>
+          </dl>
+        </div>
+        <Buttons />
+      </div >
+    )
+  }
+
   return (
     <div className="App">
-      <p>{renderedResponse}</p>
+      <MainContent>
+        {renderedResponse && renderedResponse[0] && imgCache.length > 0 && <Response />}
+      </MainContent>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const env = process.env.NODE_ENV || "development";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// dotenv
+if (env === "development") {
+  require("dotenv").config();
+}
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/api/hello", (req, res) => {
-  res.send({
-    express: "Hey from Express land"
+  return res.status(200).json({
+    message: "Hey from Express land"
   })
 });
 
 app.post("/api/world", (req, res) => {
   console.log(req.body);
-  res.send(`Received a POST request. Info: ${req.body.post}`);
+  return res.status(200).json(`Received a POST request. Info: ${req.body.post}`);
 });
+
+if (env === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+} else {
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
